86e4016431222d071f1d8e5b9fb89c1bf6d540b1
zuqiu2024 <zhanxuxiang2022@163.com>
2026-01-20 02:56:03 +0000

    Deploying to pages from @ zuqiu2024/zxx@5ab343d1e9a94928bce1d1c62ae568b4833cf803 🚀

---
+++ _astro/KatexManager.astro_astro_type_script_index_0_lang.CEbbgczo.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["_astro/katex.EofBC0DB.css"])))=>i.map(i=>d[i]);
+import{_ as t}from"./preload-helper.BlTxHScW.js";function e(){document.querySelector(".katex")&&t(()=>Promise.resolve({}),__vite__mapDeps([0]))}e();document.addEventListener("swup:content:replace",e);
---
+++ _astro/translation.C9zUeTiQ.js
@@ -0,0 +1,1 @@
+import{s as o}from"./navBarConfig.Bx7Aaq8G.js";import{r as t,j as r,z as e,a as i,e as n}from"./zh_TW.Bl-Ke2VL.js";const u=n,_={en:n,en_us:n,en_gb:n,en_au:n,zh_cn:i,zh_tw:e,ja:r,ja_jp:r,ru:t,ru_ru:t};function c(a){return _[a.toLowerCase()]||u}function l(a){const s=o.lang||"en";return c(s)[a]}export{l as i};
